7c992b3c76c68775fa00428857f9a6a607e13887
JaviCandia <javi-candia@hotmail.com>
2021-05-02 14:10:20 -0500

    Accordion redesign. Added clear fields inputs. Totals functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrderEntryComponent } from './pages/order-entry/order-entry.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/order-entry/order-entry.component.ts
+++ src/app/pages/order-entry/order-entry.component.ts
@@ -7,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderEntryComponent implements OnInit {
 
+  // Data for the basic inputs
+  public storeCode: string;
+  public requestedBy: string;
+  public email: string;
+  public phone: string;
+
+  // Content of the mid tables
   public storeInfo: Array<string>;
   public shippingInfo: Array<string>;
 
-  public rowArray: Array<any>;
+  // Just for visual purposes, delete later
+  public rowArray: Array<any> = [];
 
-  constructor() {
-    this.rowArray = [];
-  }
+  // Totals data
+  public graphicTotal: number = 0;
+  public rackTotal: number = 0;
+  public chillerTotal: number = 0;
+  public freezerTotal: number = 0;
+  public orderTotal: number = 0;
+
+  constructor() { }
   
   ngOnInit(): void {
     this.searchStore();
@@ -37,5 +50,22 @@ export class OrderEntryComponent implements OnInit {
         '98122',
       '206-802-3720',
     ];
+  }
+
+  public clearBasicFields(){
+    this.storeCode = '';
+    this.requestedBy = '';
+    this.email = '';
+    this.phone = '';
+  }
+
+  public submit(){
+  this.graphicTotal = 0;
+  this.rackTotal = 0;
+  this.chillerTotal = 0;
+  this.freezerTotal = 0;
+  this.orderTotal = 0;
+
+  this.clearBasicFields();
   }
 }
